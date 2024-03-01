c0cd2a5ff88e3b4b799c395dc1e0e26e2c2e54f6
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-01 16:10:31 +0500

    Chapter no 9-11 half complete

---
+++ app.js
@@ -0,0 +1,103 @@
+//Chapter No: 9-11
+
+//Q1
+// var cityName = prompt("Enter Your City Name:");
+// if (cityName == "Karachi" || cityName == "karachi") {
+//     alert("“Welcome to city of lights”");
+// };
+
+//Q2
+// var gender = prompt("Enter Your Gender:");
+// if (gender == "Male" || gender == "male") {
+//     alert("Good Morning Sir.");
+// } else if (gender == "Female" || gender == "female") {
+//     alert("Good Morning Ma’am.");
+// };
+
+//Q3
+// var signalColor = prompt("Enter Color of Road Traffic Signal");
+// if (signalColor == "Red" || signalColor == "red") {
+//     alert("Must Stop");
+// } else if (signalColor == "Yellow" || signalColor == "yellow") {
+//     alert("Ready to move");
+// } else if (signalColor == "Green" || signalColor == "green") {
+//     alert("Move now");
+// };
+
+//Q4
+// var fuelInCar = +prompt("Enter Your Car Fuel In Litres:");
+// if (fuelInCar >= 0.25) {
+//     alert("No Need To Refill Right Now");
+// } else if (fuelInCar <= 0.25) {
+//     alert("“Please refill the fuel in your car”");
+// };
+
+//Q5
+//a)
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// };
+
+//b)
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// };
+
+//c)
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// } else if (c === 13){
+//     alert("condition 2 is true");
+// } else if (++c < 14){
+//     alert("condition 3 is true");
+// } else if(c === 14){
+//     alert("condition 4 is true");
+// };
+
+//d)
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//     alert(totalCost );
+// }if (totalCost === laborCost + materialCost){
+//     alert(true);
+// } else {
+//     alert(false);
+// };
+
+//f)
+// var car = "car < cat";
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// };
+
+//Q6
+// var Eng = +prompt("Enter Your English Marks:");
+// var Maths = +prompt("Enter Your Maths Marks:");
+// var Urdu = +prompt("Enter Your Urdu Marks:");
+// var totalObtainedMarks = Eng + Maths + Urdu;
+// var totalMarks = 300;
+// var perc = (totalObtainedMarks / totalMarks) * 100;
+// if (perc >= 80 && perc <= 100) {
+//     alert("A-one");
+//     console.log("Excellent");
+//     document.write("<h1>Marks Sheet</h1>","</br>","</br>","Total marks : ",totalMarks,"</br>","Marks obtained : ",totalObtainedMarks,"</br>","Percentage : ",perc,"%","</br>","Grade : A-one","</br>","Remarks : Excellent");
+// } else if (perc >= 70 && perc <= 80) {
+//     alert("A");
+//     console.log("Good");
+//     document.write("<h1>Marks Sheet</h1>","</br>","</br>","Total marks : ",totalMarks,"</br>","Marks obtained : ",totalObtainedMarks,"</br>","Percentage : ",perc,"%","</br>","Grade : A","</br>","Remarks : Good");
+// } else if (perc >= 60 && perc <= 70) {
+//     alert("B");
+//     console.log("You need to improve");
+//     document.write("<h1>Marks Sheet</h1>","</br>","</br>","Total marks : ",totalMarks,"</br>","Marks obtained : ",totalObtainedMarks,"</br>","Percentage : ",perc,"%","</br>","Grade : B","</br>","Remarks : You need to improve");
+// } else if (perc <= 60) {
+//     alert("Fail");
+//     console.log("Sorry");
+//     document.write("<h1>Marks Sheet</h1>","</br>","</br>","Total marks : ",totalMarks,"</br>","Marks obtained : ",totalObtainedMarks,"</br>","Percentage : ",perc,"%","</br>","Grade : Fail","</br>","Remarks : Sorry");
+// };
+
+//Q7
